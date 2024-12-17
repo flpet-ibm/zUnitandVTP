fe005af4ea2edad17c52170384bcf296013a5cbf
FlemmingPetersen <FlemmingPetersen@IBM-PF2PD5SM>
2024-12-17 14:08:07 +0100

    ADDI samples data flow

---
+++ cobol/ADDIPGM2.cbl
@@ -0,0 +1,33 @@
+       ID DIVISION.
+       PROGRAM-ID. ADDIPGM2.
+      ***
+      *
+      *    (C) 2019 IBM FLEMMING PETERSEN
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+      *
+       01  MY-CUST.
+           COPY ADDICPY1.
+
+       01 MY-PGM PIC X(8).
+
+
+       LINKAGE SECTION.
+       01  LS-CUST.
+           COPY ADDICPY1.
+
+       PROCEDURE DIVISION USING LS-CUST.
+      *
+       MAIN SECTION.
+       MAIN1.
+
+           MOVE LS-CUST TO MY-CUST
+
+           MOVE 'ADDIPGM3' TO MY-PGM
+           CALL MY-PGM USING MY-CUST
+
+           MOVE MY-CUST TO LS-CUST.
+
+       END PROGRAM ADDIPGM2.
