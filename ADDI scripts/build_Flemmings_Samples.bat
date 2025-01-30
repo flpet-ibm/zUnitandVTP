edd9f84a6568cc8cef30f601e440a5779e0fe331
Flemming Petersen <67786397+flpet-ibm@users.noreply.github.com>
2025-01-30 09:35:38 +0100

    Update build_Flemmings_Samples.bat

--- ADDI scripts/build_Flemmings_Samples.bat
+++ ADDI scripts/build_Flemmings_Samples.bat
@@ -1,7 +1,7 @@
 @echo off
 set GITFOLDER=C:\IBM AD\Git\zUnitandVTP
 set GITURL=git@github.com:flpet-ibm/zUnitandVTP.git
-set ADDIPROJ=Flemmings_Samples
+set ADDIPROJ=TAZSamples
 set ADDIROOT=C:\IBM AD\Mainframe Projects
 set ADDIINST=C:\Program Files\IBM Application Discovery and Delivery Intelligence
 set currdir=%~dp0
@@ -23,7 +23,7 @@ echo Copying PSB Mapping file to %ADDIROOT%\%ADDIPROJ%
 copy "%GITFOLDER%\ADDI Config\PSBmap.txt" "%ADDIROOT%\%ADDIPROJ%"
 set BCCMD="%ADDIINST%\IBM Application Discovery Build Client\Bin\Release\IBMApplicationDiscoveryBuildClient"
 echo Updateing sync.ini
-powershell "& '%currdir%\updateSyncIni.ps1' Flemmings_Samples 'C:\IBM AD\Git\zUnitandVTP\' -Write"
+powershell "& '%currdir%\updateSyncIni.ps1' %ADDIPROJ% 'C:\IBM AD\Git\zUnitandVTP\' -Write"
 echo Synchronzing ADDI project %ADDIPROJ%
 %BCCMD%  /umm1 %ADDIPROJ%
 echo Performing Make for ADDI project %ADDIPROJ%
